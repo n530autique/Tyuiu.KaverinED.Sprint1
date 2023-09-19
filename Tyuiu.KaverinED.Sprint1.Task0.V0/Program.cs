78b5b5d86655d889f8138ad95af90b2c1fa67a58
egorkaverin <egorkaverin@E8E1>
2023-09-19 21:55:47 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KaverinED.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KaverinED.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.KaverinED.Sprint1.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Каверин Е. Д. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Каверин Егор Дмитриевич | ИИПб-23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 18/6*2-(4-3)            *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:  18/6*2-(4-3)                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"* РЕЗУЛЬТАТ: {ds.Calculate()}                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.ReadLine();
+        }
+    }
+}
