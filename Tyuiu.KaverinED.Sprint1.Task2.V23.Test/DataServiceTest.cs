8eca8fd2b6a53a1770c3ef0e77f93644c2e61938
Каверин Егор <Tyuiu.KaverinED@outlook.com>
2023-09-20 17:49:03 +0500

    Task2

---
+++ Tyuiu.KaverinED.Sprint1.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KaverinED.Sprint1.Task2.V23.Lib;
+namespace Tyuiu.KaverinED.Sprint1.Task2.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int res = x * 60;
+            Assert.AreEqual(res, 120);
+        }
+    }
+}
---
+++ Tyuiu.KaverinED.Sprint1.Task2.V23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KaverinED.Sprint1.Task2.V23.Lib;
+namespace Tyuiu.KaverinED.Sprint1.Task2.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Каверин Е. Д. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Каверин Егор Дмитриевич | ИИПб-23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Введите количество минут: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"* РЕЗУЛЬТАТ: {x} мин. = {ds.ConvertMinutesToSeconds(x)} сек.");
+            Console.WriteLine("***************************************************************************");
+            Console.ReadLine();
+        }
+    }
+}
