8cd309a1a7b4849a6af2753f643b77b181d24d43
Каверин Егор <Tyuiu.KaverinED@outlook.com>
2023-09-20 18:48:57 +0500

    Task3

---
+++ Tyuiu.KaverinED.Sprint1.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.KaverinED.Sprint1.Task3.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Validexpression()
+            
+        {
+            int a = 4, b = 5, c = 2;
+            int res = (a / c) * (b / c);
+            Assert.AreEqual(res, 4);
+        }
+    }
+}
