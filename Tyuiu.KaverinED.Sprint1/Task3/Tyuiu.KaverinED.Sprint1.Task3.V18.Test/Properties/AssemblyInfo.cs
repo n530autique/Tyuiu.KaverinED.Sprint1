fdbf82224d09b5ae17006011aea64518059d760b
Каверин Егор <Tyuiu.KaverinED@outlook.com>
2023-09-20 19:10:48 +0500

    Task3

---
+++ Tyuiu.KaverinED.Sprint1/Task3/Tyuiu.KaverinED.Sprint1.Task3.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KaverinED.Sprint1.Task2.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KaverinED.Sprint1.Task2.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb4291e3-94d6-4a86-b22e-9bd8336589e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
